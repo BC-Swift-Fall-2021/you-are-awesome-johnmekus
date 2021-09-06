db06b736ceb901e80c0e6d7c82e3b45eb63b2808
johnmekus <mekus@bc.edu>
2021-09-06 12:56:46 -0400

    2.7

--- You Are Awesome!/FirstApp/ViewController.swift
+++ You Are Awesome!/FirstApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        awesomeLabel.text = ""
         // Do any additional setup after loading the view.
         
         //print("viewDidLoad has executed!")
