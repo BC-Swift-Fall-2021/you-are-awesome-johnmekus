80594e8e16b634eedd526c2a0a16c2dfc436b708
johnmekus <mekus@bc.edu>
2021-09-06 12:22:18 -0400

    2.6

--- You Are Awesome!/FirstApp/ViewController.swift
+++ You Are Awesome!/FirstApp/ViewController.swift
@@ -9,18 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var awesomeLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("viewDidLoad has executed!")
+        //print("viewDidLoad has executed!")
     }
 
 
     @IBAction func andyChapmanButtonPressed(_ sender: UIButton) {
-        print("It was pressed.")
+        //print("It was pressed.")
         awesomeLabel.text = "You are awesome!"
+        imageView.image = UIImage(named: "image6")
     }
 }
 
